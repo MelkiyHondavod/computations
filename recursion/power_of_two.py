5b2328386538c8769635375cad9833e756318396
Nikita Kabeshev <>
2021-04-12 14:36:01 +0000

    is_power_of_two is done

--- recursion/power_of_two.py
+++ recursion/power_of_two.py
@@ -1,5 +1,15 @@
 #Дано натуральное число N. Выведите слово YES, если число N является точной степенью двойки, или слово NO в противном случае.
 #Операцией возведения в степень пользоваться нельзя!
 
-def is_power_of_two(N):
-    return 'YES'
+def is_power_of_two(N, power=1):
+	if power == N:
+		return "YES"
+	if power > N:
+		return "NO"
+	power = power*2	 
+	return is_power_of_two(N,power)
+
+number=int(input("Enter number \n"))	
+print(is_power_of_two(number))
+
+  
